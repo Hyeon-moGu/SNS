02598b960c394af6d60f444c4fc045cc03debeb2
Hyeonmo <khm052678@gmail.com>
2022-09-27 18:03:38 +0900

    Post 작성 기능 추가
    Request 파일들을 리팩토링하고 Post작성을 위한 코드들을 작성 및 수정함

---
+++ src/main/java/com/simplesns/sns/controller/request/UserJoinRequest.java
@@ -0,0 +1,13 @@
+package com.simplesns.sns.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserJoinRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+}
