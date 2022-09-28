9378988ec1c1125c076736acebd22b8c32bd501c
Hyeonmo <khm052678@gmail.com>
2022-09-28 14:49:18 +0900

    Refact : 변수명 변경
    Front-end에 `username`으로 요청하면 User_Not_Found 오류 발생하여 프론트 코드와 같은 이름 `user`로 수정함

--- src/main/java/com/simplesns/sns/controller/request/UserJoinRequest.java
+++ src/main/java/com/simplesns/sns/controller/request/UserJoinRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UserJoinRequest {
-    private String username;
+    private String name;
     private String password;
     private String email;
     private String nickname;
